bfbc3eb614d21db0b383608944c4565a6c93d3d9
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2021-12-20 21:03:11 +0300

    Add Factory Method in SceneDelegate to instantiate `AmountViewController`

--- Fight with daemon/AmountViewController.swift
+++ Fight with daemon/AmountViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AmountViewController.swift
 //  Fight with daemon
 //
 //  Created by Mohammad Bitar on 12/20/21.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class AmountViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
-
