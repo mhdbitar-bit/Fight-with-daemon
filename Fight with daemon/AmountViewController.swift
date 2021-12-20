119a5e240465dcbe189e6036bd117ad42a2bd6ca
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2021-12-20 21:30:11 +0300

    Add TextField extension to handle the publishing of text using `Combine Framework`

--- Fight with daemon/AmountViewController.swift
+++ Fight with daemon/AmountViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 final class AmountViewController: UIViewController {
     
---
+++ Fight with daemon/Extensions/UITextField+Publisher.swift
@@ -0,0 +1,20 @@
+//
+//  UITextField+Publisher.swift
+//  Fight with daemon
+//
+//  Created by Mohammad Bitar on 12/20/21.
+//
+
+import UIKit
+import Combine
+
+extension UITextField {
+    
+    var textPublisher: AnyPublisher<String, Never> {
+        NotificationCenter.default
+            .publisher(for: UITextField.textDidChangeNotification, object: self)
+            .compactMap { $0.object as? UITextField }
+            .compactMap(\.text)
+            .eraseToAnyPublisher()
+    }
+}
