a56f251eb438c356c313bf9017007d844c170e1d
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2021-12-22 21:37:00 +0300

    Add Deamon Model

---
+++ Fight with daemon/Data/Deamon.swift
@@ -0,0 +1,14 @@
+//
+//  Deamon.swift
+//  Fight with daemon
+//
+//  Created by Mohammad Bitar on 12/22/21.
+//
+
+import Foundation
+
+public struct Deamon: Equatable {
+    let name: String
+    let image: String
+    let powers: [Power]
+}
--- Fight with daemon/Data/Weapon.swift
+++ Fight with daemon/Data/Weapon.swift
@@ -7,16 +7,9 @@
 
 import Foundation
 
-enum WeaponPowers {
-    case Water
-    case Fire
-    case Ice
-    case Thunder
-}
-
 public struct Weapon: Equatable {
     let name: String
     let price: Int
     let image: String
-    let power: WeaponPowers
+    let power: Power
 }
